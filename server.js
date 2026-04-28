b24e99280196a6ce28bbae25a1af966a666b2878
punsak1998 <punsak1998@gmail.com>
2026-04-28 17:32:24 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -19,9 +19,10 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) { cb(null, 'uploads/'); },
   filename: function (req, file, cb) { cb(null, Date.now() + path.extname(file.originalname)); }
 });
+
 // 🟢 ตั้งค่าระบบอัปโหลดรูป พร้อมจำกัดขนาดสูงสุด 5MB
 const upload = multer({ 
-  dest: 'uploads/',
+  storage: storage, // <--- แก้ตรงนี้ครับ! เปลี่ยนจาก dest: 'uploads/' เป็น storage: storage
   limits: { 
     fileSize: 5 * 1024 * 1024 // 5 MB (คำนวณเป็นหน่วย Byte)
   },
